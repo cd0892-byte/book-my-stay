bb7ab36989e4c36e23422eeb830e9d9bca3e2b1b
cd0892-byte <cd0892@srmist.edu.in>
2026-03-25 15:14:28 +0530

    UseCase1 book my stay

---
+++ UC1.java
@@ -0,0 +1,18 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * Entry point for the Hotel Booking Management System.
+ *
+ * @author D CHARAN SAI REDDY
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=====================================");
+        System.out.println("Your journey to comfortable stays begins here!");
+    }
+}
